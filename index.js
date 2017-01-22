0caec0f7671f34ef62d1d4ef4eb46865b267aadd
Sean Wilson <hello@imsean.me>
2017-01-22 01:39:13 -0600

    remove repeat condition

--- index.js
+++ index.js
@@ -4,9 +4,8 @@ const repeatString = require('repeat-string')
 
 const list = exports.list = function list(struct) {
   const signature = traverse(struct).reduce(function(acc, x) {
-    const repeat = this.isRoot ? '#' : '#'
     const reset = this.isLeaf ? '\r\n' : ''
-    if (!Array.isArray(x)) acc += repeatString(repeat, this.level) + x + reset
+    if (!Array.isArray(x)) acc += repeatString('#', this.level) + x + reset
     return acc
   }, '')
   return signature.trim()
